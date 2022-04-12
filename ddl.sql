0735cae790542bbde72dec69e619bd1980e18f49
LAPTOP-PKB17DK0\duszy <pawel.duszyn@student.wat.edu.pl>
2022-04-12 09:45:47 +0200

    Update existing tables and relation in HR department and creating tables and relations in prodcution set-up department

--- ddl.sql
+++ ddl.sql
@@ -6,17 +6,32 @@ CREATE DATABASE KWZP
 GO
 USE KWZP
 
+CREATE TABLE Pracownicy(
+	ID_pracownik INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Nazwisko VARCHAR(50) NOT NULL,
+	Imie VARCHAR(50) NOT NULL,
+	Nr_dowodu VARCHAR(9) NOT NULL,
+	Pesel INT NOT NULL,
+	Wyksztalcenie VARCHAR(10) NOT NULL,
+	-- ID_umowa INT FOREIGN KEY REFERENCES Umowa(ID_umowa),
+)
 CREATE TABLE Dane_adresowe_pracownik(
 	ID_dane_adresowe_pracownik INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_pracownik INT FOREIGN KEY 
+		REFERENCES Pracownicy(ID_pracownik) NOT NULL,
 	Miejscowosc VARCHAR(50) NOT NULL,
 	Ulica VARCHAR(50),
 	Nr_budynku VARCHAR(10)NOT NULL,
 	Nr_lokalu VARCHAR(10),
-	Kod_pocztowy VARCHAR(5) NOT NULL
+	Kod_pocztowy VARCHAR(5) NOT NULL,
+	Data_od DATE NOT NULL,
+	Data_do DATE
 )
 
 CREATE TABLE Nr_telefon_pracownik(
-	ID_nr_telefon_pracownik int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_numer_telefonu INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_pracownik INT FOREIGN KEY 
+		REFERENCES Pracownicy(ID_pracownik) NOT NULL,
 	Numer VARCHAR(20) NOT NULL,
 	Data_od DATE NOT NULL,
 	Data_do DATE
@@ -24,6 +39,8 @@ CREATE TABLE Nr_telefon_pracownik(
 
 CREATE TABLE Email_pracownik(
 	ID_email_pracownik INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_pracownik INT FOREIGN KEY 
+		REFERENCES Pracownicy(ID_pracownik) NOT NULL,
 	Email VARCHAR(100) NOT NULL,
 	Data_od DATE NOT NULL,
 	Data_do DATE
@@ -31,35 +48,12 @@ CREATE TABLE Email_pracownik(
 
 CREATE TABLE Stanowisko(
 	ID_stanowisko INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	Nazwa VARCHAR(50) NOT NULL
+	Nazwa_stanowiska VARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Dzial(
 	ID_dzial INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	Nazwa VARCHAR(50) NOT NULL
-)
-
-CREATE TABLE Stanowisko_dzial(
-	ID_stanowisko_dzial INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	ID_stanowisko INT FOREIGN KEY 
-		REFERENCES Stanowisko(ID_stanowisko) NOT NULL,
-	ID_dzial INT FOREIGN KEY 
-		REFERENCES Dzial(ID_dzial) NOT NULL
-)
-
-CREATE TABLE Pracownicy(
-	ID_pracownik INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	Nazwisko VARCHAR(50) NOT NULL,
-	Imie VARCHAR(50) NOT NULL,
-	ID_dane_adresowe int FOREIGN KEY 
-		REFERENCES Dane_adresowe_pracownik(ID_dane_adresowe_pracownik),
-	Nr_dowodu VARCHAR(9) NOT NULL,
-	Pesel VARCHAR(10) NOT NULL,
-	ID_nr_telefon INT FOREIGN KEY 
-		REFERENCES Nr_telefon_pracownik(ID_nr_telefon_pracownik),
-	ID_email_pracownik INT FOREIGN KEY 
-	REFERENCES Email_pracownik(ID_email_pracownik)
-	-- ID_umowa INT FOREIGN KEY REFERENCES Umowa(ID_umowa),
+	Nazwa_dzialu VARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Rodzaj_kontroli(
@@ -245,17 +239,63 @@ ID_wymienione_czesci INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 ID_czesci INT FOREIGN KEY
 	REFERENCES Czesci(ID_czesci)
 )
-
-CREATE TABLE Posada_pracownik(
+CREATE TABLE Etat(
+	ID_etat INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_stanowisko INT FOREIGN KEY 
+		REFERENCES Stanowisko(ID_stanowisko) NOT NULL,
+	ID_dzial INT FOREIGN KEY 
+		REFERENCES Dzial(ID_dzial) NOT NULL,
+	Liczba_miejsc INT NOT NULL,
+	)
+CREATE TABLE Posada_pracownika(
 	ID_posada INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	ID_pracownik INT FOREIGN KEY 
-		REFERENCES Pracownicy(ID_pracownik) NOT NULL,
-	ID_stanowisko_dzial int FOREIGN KEY 
-		REFERENCES Stanowisko_dzial(ID_stanowisko_dzial) NOT NULL,
+	ID_etat INT FOREIGN KEY
+	REFERENCES Etat(ID_etat),
 	Data_od DATE NOT NULL,
 	Data_do DATE
 	)
+CREATE TABLE Rodzaj_umowy(
+	ID_rodzaj_umowy INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Nazwa NVARCHAR(50) NOT NULL,
+	Skrot NVARCHAR(10) NOT NULL,
+	)
 
+CREATE TABLE Rodzaj_zatrudnienia(
+	ID_rodzaj_zatrudnienia INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_rodzaj_umowy INT FOREIGN KEY
+	REFERENCES Rodzaj_umowy(ID_rodzaj_umowy),
+	Przyslugujacy_urlop INT NOT NULL,
+	)
+CREATE TABLE Rodzaj_nieobecnosci(
+	ID_rodzaj_nieobecnosci INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Symbol NVARCHAR(10) NOT NULL,
+	)
+CREATE TABLE Nieobecnosc(
+	ID_nieobecnosc INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_pracownik INT FOREIGN KEY
+	REFERENCES Pracownicy(ID_pracownik),
+	ID_rodzaj_nieobecnosci  INT FOREIGN KEY
+	REFERENCES Rodzaj_nieobecnosci(ID_rodzaj_nieobecnosci),
+	Data_od DATE NOT NULL,
+	Data_do DATE NOT NULL,
+	)
+CREATE TABLE Wymiar_pracy(
+ID_wymiar_pracy INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+Nazwa NVARCHAR(50) NOT NULL,
+	)
+
+CREATE TABLE Umowa(
+	ID_umowa INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	ID_wymiar_pracy INT FOREIGN KEY
+	REFERENCES Wymiar_pracy(ID_wymiar_pracy),
+	ID_rodzaj_zatrudnienia INT FOREIGN KEY
+	REFERENCES Rodzaj_zatrudnienia(ID_rodzaj_zatrudnienia),
+	ID_pracownik INT FOREIGN KEY
+	REFERENCES Pracownicy(ID_pracownik),
+	ID_posada INT FOREIGN KEY
+	REFERENCES Posada_pracownika(ID_posada),
+	Wynagrodzenie INT NOT NULL,
+	)
 CREATE TABLE Klient
 	(
 	ID_klient INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
@@ -586,4 +626,119 @@ ID_producent INT FOREIGN KEY REFERENCES Producenci(ID_producent) NOT NULL,
 Ilosc INT NOT NULL,
 Cena DECIMAL(15,2) NOT NULL,
 CONSTRAINT PK_SzczegolyZCZ PRIMARY KEY (ID_zamowienia_czesci, ID_czesci, ID_producent)
+);
+CREATE TABLE PT_Produkt(
+ID_produkt INT PRIMARY KEY NOT NULL,
+ID_proces_technologiczny INT NOT NULL,
+Nazwa_produktu NVarchar(25) NOT NULL,
+Cena_produktu Money NOT NULL
+);
+CREATE TABLE Dokumentacja(
+ID_dokumentacja INT PRIMARY KEY NOT NULL,
+ID_produkt INT FOREIGN KEY REFERENCES PT_Produkt(ID_produkt) NOT NULL,
+Data Date NOT NULL
+);
+CREATE TABLE Zamowienie_dokumentacja(
+ID_zamowienie INT FOREIGN KEY 
+	REFERENCES Dokumentacja(ID_dokumentacja) NOT NULL,
+ID_dokumentacja INT FOREIGN KEY 
+	REFERENCES Dokumentacja(ID_dokumentacja) NOT NULL,
+);
+
+
+
+
+CREATE TABLE PT_Parametr_produkt(
+ID_parametr INT PRIMARY KEY NOT NULL,
+Nazwa NVarchar(50)NOT NULL,
+Wartosc_dolna INT NOT NULL,
+Nominal INT NOT NULL,
+Wartosc_gorna INT NOT NULL
+);
+
+CREATE TABLE PT_Szczegoly_produkt(
+ID_produkt INT FOREIGN KEY REFERENCES PT_Produkt(ID_produkt) NOT NULL,
+ID_parametr INT FOREIGN KEY REFERENCES PT_Parametr_produkt(ID_parametr) NOT NULL
+);
+
+CREATE TABLE PT_Slownik_polprodukt(
+ID_polprodukt INT PRIMARY KEY NOT NULL,
+Nazwa NVarchar(25) NOT NULL
+);
+
+CREATE TABLE PT_Sklad_produkt(
+ID_produkt INT FOREIGN KEY REFERENCES PT_Produkt(ID_produkt) NOT NULL,
+ID_polprodukt INT FOREIGN KEY REFERENCES PT_Slownik_polprodukt(ID_polprodukt) NOT NULL,
+Ilosc INT NOT NULL
+);
+CREATE TABLE PT_polprodukt(
+ID_polprodukt INT PRIMARY KEY NOT NULL,
+Nazwa NVarchar(50) NOT NULL,
+)
+CREATE TABLE PT_Sklad_polprdukt(
+ID_polprodukt INT FOREIGN KEY REFERENCES PT_polprodukt(ID_polprodukt) NOT NULL,
+ID_material INT NOT NULL,
+Masa INT NOT NULL
+);
+
+CREATE TABLE PT_Parametr_polprodukt(
+ID_parametr INT PRIMARY KEY NOT NULL,
+Nazwa NVarchar(50) NOT NULL,
+Wartosc_dolna INT NOT NULL,
+Nominal INT NOT NULL,
+Wartosc_gorna INT NOT NULL
+);
+
+
+CREATE TABLE PT_Szczegoly_polprodukt(
+ID_polprodukt INT  FOREIGN KEY REFERENCES PT_polprodukt(ID_polprodukt) NOT NULL,
+ID_parametr INT FOREIGN KEY REFERENCES PT_Parametr_polprodukt(ID_parametr) NOT NULL
+CONSTRAINT PT_SzczegolyPP PRIMARY KEY (ID_polprodukt, ID_parametr)
+);
+
+CREATE TABLE PT_Slownik_stanowisko(
+ID_nazwa_stanowiska INT PRIMARY KEY NOT NULL,
+Nazwa_stanowiska NVarchar(20) NOT NULL
+);
+
+CREATE TABLE PT_Stanowisko_produkcyjne(
+ID_stanowisko_produkcyjne INT PRIMARY KEY NOT NULL,
+ID_maszyny INT NOT NULL,
+ID_nazwa_stanowiska INT FOREIGN KEY REFERENCES PT_Slownik_stanowisko(ID_nazwa_stanowiska) NOT NULL
+);
+
+CREATE TABLE PT_Czynnosc_produkcyjna(
+ID_czynnosc_produkcyjna INT PRIMARY KEY NOT NULL,
+Nazwa NVarchar(50) NOT NULL
+);
+
+CREATE TABLE PT_Sklad_stanowisko_produkcyjne(
+ID_stanowisko_produkcyjne INT FOREIGN KEY REFERENCES PT_Stanowisko_produkcyjne(ID_stanowisko_produkcyjne) NOT NULL,
+ID_narzedzia INT NOT NULL
+);
+
+CREATE TABLE PT_Sklad_stanowisko_produkcyjne_maszyny(
+ID_stanowisko_produkcyjne INT FOREIGN KEY REFERENCES PT_Stanowisko_produkcyjne(ID_stanowisko_produkcyjne) NOT NULL,
+ID_maszyny INT NOT NULL
+);
+
+
+CREATE TABLE PT_Etap_produkcji(
+ID_etap_produkcji INT PRIMARY KEY NOT NULL,
+ID_stanowisko_produkcyjne INT FOREIGN KEY REFERENCES PT_stanowisko_produkcyjne(ID_stanowisko_produkcyjne) NOT NULL,
+ID_czynnosc_produkcyjna INT FOREIGN KEY REFERENCES PT_Czynnosc_produkcyjna(ID_czynnosc_produkcyjna) NOT NULL
+);
+
+CREATE TABLE Etap_proces_technologiczny(
+ID_etap_proces_technologiczny INT PRIMARY KEY NOT NULL,
+ID_pracownik INT NOT NULL,
+ID_etap_produkcji INT FOREIGN KEY REFERENCES PT_Etap_produkcji(ID_etap_produkcji) NOT NULL,
+Czas_od Date NOT NULL,
+Czas_do Date
+);
+
+CREATE TABLE PP_proces_technologiczny(
+ID_etap_proces_technologiczny INT FOREIGN KEY REFERENCES Etap_proces_technologiczny(ID_etap_proces_technologiczny) NOT NULL,
+ID_polprodukt INT FOREIGN KEY 
+	REFERENCES PT_polprodukt(ID_polprodukt),
 );
