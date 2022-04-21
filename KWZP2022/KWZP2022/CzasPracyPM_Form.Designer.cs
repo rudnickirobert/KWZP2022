65999f9a9a59c79ff9c84ffd0dc83c47838509ed
WML25\68779SP <szymon.paryszewski@student.wat.edu.pl>
2022-04-21 12:06:07 +0200

    Added form CzasPracyPM_Form

---
+++ KWZP2022/KWZP2022/CzasPracyPM_Form.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace KWZP2022
+{
+    partial class CzasPracyPM_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CzasPracyPM_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CzasPracyPM_Form";
+            this.Text = "Ewidencja czasu pracy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ KWZP2022/KWZP2022/CzasPracyPM_Form.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWZP2022
+{
+    public partial class CzasPracyPM_Form : Form
+    {
+        KWZPEntities db;
+        public CzasPracyPM_Form(KWZPEntities db)
+        {
+            InitializeComponent();
+            this.db = db;
+        }
+    }
+}
