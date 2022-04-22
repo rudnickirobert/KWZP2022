857c13a70a6bdee7d7ac3b3dba5eebfcf1f4e2f3
LAPTOP-P242TEH4\Mateusz <mateusz.gawron1998@gmail.com>
2022-04-22 23:54:16 +0200

    Edited form FormSetupProductionDepartment

--- KWZP2022/KWZP2022/FormSetupProductionDepartment.Designer.cs
+++ KWZP2022/KWZP2022/FormSetupProductionDepartment.Designer.cs
@@ -30,22 +30,318 @@ namespace KWZP2022
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSetupProductionDepartment));
+            this.dtbDataStart = new System.Windows.Forms.DataGridView();
+            this.txtProdukt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCzynnoscProdukcyjna = new System.Windows.Forms.TextBox();
+            this.taxt2 = new System.Windows.Forms.Label();
+            this.txtPracownik = new System.Windows.Forms.TextBox();
+            this.txt = new System.Windows.Forms.Label();
+            this.txtStanowiskoProdukcyjne = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSzacowanyCzas = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnAktualizujProces = new System.Windows.Forms.Button();
+            this.btnDodajPrcoes = new System.Windows.Forms.Button();
+            this.cbProdukt = new System.Windows.Forms.ComboBox();
+            this.cbCzynnoscProdukcyjna = new System.Windows.Forms.ComboBox();
+            this.cbPracownik = new System.Windows.Forms.ComboBox();
+            this.cbStanowiskoProdukcyjne = new System.Windows.Forms.ComboBox();
+            this.cbSzacownyCzas = new System.Windows.Forms.ComboBox();
+            this.cbDataRozpoczecia = new System.Windows.Forms.ComboBox();
+            this.cbDataZakonczenia = new System.Windows.Forms.ComboBox();
+            this.dtbDataEnd = new System.Windows.Forms.DateTimePicker();
+            this.dtbDataRozpoczecia = new System.Windows.Forms.DateTimePicker();
+            this.txtImiePracownik = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtbDataStart)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dtbDataStart
+            // 
+            this.dtbDataStart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtbDataStart.Location = new System.Drawing.Point(36, 285);
+            this.dtbDataStart.Name = "dtbDataStart";
+            this.dtbDataStart.RowTemplate.Height = 24;
+            this.dtbDataStart.Size = new System.Drawing.Size(1448, 449);
+            this.dtbDataStart.TabIndex = 0;
+            // 
+            // txtProdukt
+            // 
+            this.txtProdukt.Location = new System.Drawing.Point(36, 50);
+            this.txtProdukt.Name = "txtProdukt";
+            this.txtProdukt.Size = new System.Drawing.Size(201, 22);
+            this.txtProdukt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Produkt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(32, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Czynność produkcyjna";
+            // 
+            // txtCzynnoscProdukcyjna
+            // 
+            this.txtCzynnoscProdukcyjna.Location = new System.Drawing.Point(36, 138);
+            this.txtCzynnoscProdukcyjna.Name = "txtCzynnoscProdukcyjna";
+            this.txtCzynnoscProdukcyjna.Size = new System.Drawing.Size(201, 22);
+            this.txtCzynnoscProdukcyjna.TabIndex = 3;
+            // 
+            // taxt2
+            // 
+            this.taxt2.AutoSize = true;
+            this.taxt2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.taxt2.Location = new System.Drawing.Point(32, 192);
+            this.taxt2.Name = "taxt2";
+            this.taxt2.Size = new System.Drawing.Size(101, 23);
+            this.taxt2.TabIndex = 6;
+            this.taxt2.Text = "Pracownik";
+            // 
+            // txtPracownik
+            // 
+            this.txtPracownik.Location = new System.Drawing.Point(36, 218);
+            this.txtPracownik.Name = "txtPracownik";
+            this.txtPracownik.Size = new System.Drawing.Size(201, 22);
+            this.txtPracownik.TabIndex = 5;
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txt.Location = new System.Drawing.Point(362, 24);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(222, 23);
+            this.txt.TabIndex = 8;
+            this.txt.Text = "Stanowisko produkcyjne";
+            // 
+            // txtStanowiskoProdukcyjne
+            // 
+            this.txtStanowiskoProdukcyjne.Location = new System.Drawing.Point(366, 50);
+            this.txtStanowiskoProdukcyjne.Name = "txtStanowiskoProdukcyjne";
+            this.txtStanowiskoProdukcyjne.Size = new System.Drawing.Size(201, 22);
+            this.txtStanowiskoProdukcyjne.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(362, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Szacowany czas";
+            // 
+            // txtSzacowanyCzas
+            // 
+            this.txtSzacowanyCzas.Location = new System.Drawing.Point(366, 138);
+            this.txtSzacowanyCzas.Name = "txtSzacowanyCzas";
+            this.txtSzacowanyCzas.Size = new System.Drawing.Size(201, 22);
+            this.txtSzacowanyCzas.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(362, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Data rozpoczęcia";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(692, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(164, 23);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Data zakończenia";
+            // 
+            // btnAktualizujProces
+            // 
+            this.btnAktualizujProces.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnAktualizujProces.Font = new System.Drawing.Font("Arial", 15F);
+            this.btnAktualizujProces.Location = new System.Drawing.Point(1039, 194);
+            this.btnAktualizujProces.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAktualizujProces.Name = "btnAktualizujProces";
+            this.btnAktualizujProces.Size = new System.Drawing.Size(445, 46);
+            this.btnAktualizujProces.TabIndex = 22;
+            this.btnAktualizujProces.Text = "Aktualizacja";
+            this.btnAktualizujProces.UseVisualStyleBackColor = false;
+            // 
+            // btnDodajPrcoes
+            // 
+            this.btnDodajPrcoes.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnDodajPrcoes.Font = new System.Drawing.Font("Arial", 15F);
+            this.btnDodajPrcoes.Location = new System.Drawing.Point(1039, 109);
+            this.btnDodajPrcoes.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDodajPrcoes.Name = "btnDodajPrcoes";
+            this.btnDodajPrcoes.Size = new System.Drawing.Size(445, 46);
+            this.btnDodajPrcoes.TabIndex = 21;
+            this.btnDodajPrcoes.Text = "Dodaj nowy proces";
+            this.btnDodajPrcoes.UseVisualStyleBackColor = false;
+            this.btnDodajPrcoes.Click += new System.EventHandler(this.btnDodajPrcoes_Click);
+            // 
+            // cbProdukt
+            // 
+            this.cbProdukt.FormattingEnabled = true;
+            this.cbProdukt.Location = new System.Drawing.Point(36, 78);
+            this.cbProdukt.Name = "cbProdukt";
+            this.cbProdukt.Size = new System.Drawing.Size(201, 24);
+            this.cbProdukt.TabIndex = 23;
+            // 
+            // cbCzynnoscProdukcyjna
+            // 
+            this.cbCzynnoscProdukcyjna.FormattingEnabled = true;
+            this.cbCzynnoscProdukcyjna.Location = new System.Drawing.Point(36, 166);
+            this.cbCzynnoscProdukcyjna.Name = "cbCzynnoscProdukcyjna";
+            this.cbCzynnoscProdukcyjna.Size = new System.Drawing.Size(201, 24);
+            this.cbCzynnoscProdukcyjna.TabIndex = 24;
+            // 
+            // cbPracownik
+            // 
+            this.cbPracownik.FormattingEnabled = true;
+            this.cbPracownik.Location = new System.Drawing.Point(36, 246);
+            this.cbPracownik.Name = "cbPracownik";
+            this.cbPracownik.Size = new System.Drawing.Size(201, 24);
+            this.cbPracownik.TabIndex = 25;
+            // 
+            // cbStanowiskoProdukcyjne
+            // 
+            this.cbStanowiskoProdukcyjne.FormattingEnabled = true;
+            this.cbStanowiskoProdukcyjne.Location = new System.Drawing.Point(366, 78);
+            this.cbStanowiskoProdukcyjne.Name = "cbStanowiskoProdukcyjne";
+            this.cbStanowiskoProdukcyjne.Size = new System.Drawing.Size(201, 24);
+            this.cbStanowiskoProdukcyjne.TabIndex = 26;
+            // 
+            // cbSzacownyCzas
+            // 
+            this.cbSzacownyCzas.FormattingEnabled = true;
+            this.cbSzacownyCzas.Location = new System.Drawing.Point(366, 165);
+            this.cbSzacownyCzas.Name = "cbSzacownyCzas";
+            this.cbSzacownyCzas.Size = new System.Drawing.Size(201, 24);
+            this.cbSzacownyCzas.TabIndex = 27;
+            // 
+            // cbDataRozpoczecia
+            // 
+            this.cbDataRozpoczecia.FormattingEnabled = true;
+            this.cbDataRozpoczecia.Location = new System.Drawing.Point(366, 246);
+            this.cbDataRozpoczecia.Name = "cbDataRozpoczecia";
+            this.cbDataRozpoczecia.Size = new System.Drawing.Size(201, 24);
+            this.cbDataRozpoczecia.TabIndex = 28;
+            // 
+            // cbDataZakonczenia
+            // 
+            this.cbDataZakonczenia.FormattingEnabled = true;
+            this.cbDataZakonczenia.Location = new System.Drawing.Point(696, 78);
+            this.cbDataZakonczenia.Name = "cbDataZakonczenia";
+            this.cbDataZakonczenia.Size = new System.Drawing.Size(201, 24);
+            this.cbDataZakonczenia.TabIndex = 29;
+            // 
+            // dtbDataEnd
+            // 
+            this.dtbDataEnd.Location = new System.Drawing.Point(696, 50);
+            this.dtbDataEnd.Name = "dtbDataEnd";
+            this.dtbDataEnd.Size = new System.Drawing.Size(200, 22);
+            this.dtbDataEnd.TabIndex = 30;
+            // 
+            // dtbDataRozpoczecia
+            // 
+            this.dtbDataRozpoczecia.Location = new System.Drawing.Point(367, 218);
+            this.dtbDataRozpoczecia.Name = "dtbDataRozpoczecia";
+            this.dtbDataRozpoczecia.Size = new System.Drawing.Size(200, 22);
+            this.dtbDataRozpoczecia.TabIndex = 31;
+            // 
+            // txtImiePracownik
+            // 
+            this.txtImiePracownik.Location = new System.Drawing.Point(383, -1);
+            this.txtImiePracownik.Name = "txtImiePracownik";
+            this.txtImiePracownik.Size = new System.Drawing.Size(201, 22);
+            this.txtImiePracownik.TabIndex = 32;
             // 
             // FormSetupProductionDepartment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1264, 711);
+            this.ClientSize = new System.Drawing.Size(1685, 875);
+            this.Controls.Add(this.txtImiePracownik);
+            this.Controls.Add(this.dtbDataRozpoczecia);
+            this.Controls.Add(this.dtbDataEnd);
+            this.Controls.Add(this.cbDataZakonczenia);
+            this.Controls.Add(this.cbDataRozpoczecia);
+            this.Controls.Add(this.cbSzacownyCzas);
+            this.Controls.Add(this.cbStanowiskoProdukcyjne);
+            this.Controls.Add(this.cbPracownik);
+            this.Controls.Add(this.cbCzynnoscProdukcyjna);
+            this.Controls.Add(this.cbProdukt);
+            this.Controls.Add(this.btnAktualizujProces);
+            this.Controls.Add(this.btnDodajPrcoes);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSzacowanyCzas);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.txtStanowiskoProdukcyjne);
+            this.Controls.Add(this.taxt2);
+            this.Controls.Add(this.txtPracownik);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCzynnoscProdukcyjna);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProdukt);
+            this.Controls.Add(this.dtbDataStart);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormSetupProductionDepartment";
             this.Text = "Dział Przygotowania Produkcji";
+            ((System.ComponentModel.ISupportInitialize)(this.dtbDataStart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dtbDataStart;
+        private System.Windows.Forms.TextBox txtProdukt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCzynnoscProdukcyjna;
+        private System.Windows.Forms.Label taxt2;
+        private System.Windows.Forms.TextBox txtPracownik;
+        private System.Windows.Forms.Label txt;
+        private System.Windows.Forms.TextBox txtStanowiskoProdukcyjne;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSzacowanyCzas;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnAktualizujProces;
+        private System.Windows.Forms.Button btnDodajPrcoes;
+        private System.Windows.Forms.ComboBox cbProdukt;
+        private System.Windows.Forms.ComboBox cbCzynnoscProdukcyjna;
+        private System.Windows.Forms.ComboBox cbPracownik;
+        private System.Windows.Forms.ComboBox cbStanowiskoProdukcyjne;
+        private System.Windows.Forms.ComboBox cbSzacownyCzas;
+        private System.Windows.Forms.ComboBox cbDataRozpoczecia;
+        private System.Windows.Forms.ComboBox cbDataZakonczenia;
+        private System.Windows.Forms.DateTimePicker dtbDataEnd;
+        private System.Windows.Forms.DateTimePicker dtbDataRozpoczecia;
+        private System.Windows.Forms.TextBox txtImiePracownik;
     }
 }
--- KWZP2022/KWZP2022/FormSetupProductionDepartment.cs
+++ KWZP2022/KWZP2022/FormSetupProductionDepartment.cs
@@ -17,6 +17,71 @@ namespace KWZP2022
         {
             InitializeComponent();
             this.db = db;
+            showData();
+            cleanTextBox();
+            //ComboBox();
         }
+        private void showData()
+        {
+            dtbDataStart.DataSource = db.v_Proces_wytwarzanie_produkt.ToList();
+            this.dtbDataStart.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        private void cleanTextBox()
+        {
+            txtCzynnoscProdukcyjna.Clear();
+            txtPracownik.Clear();
+            txtProdukt.Clear();
+            txtStanowiskoProdukcyjne.Clear();
+            txtSzacowanyCzas.Clear();
+            dtbDataRozpoczecia.ResetText();
+            dtbDataEnd.ResetText();
+        }
+        private void btnDodajPrcoes_Click(object sender, EventArgs e)
+        {
+            if (txtProdukt.Text.Length > 0 && txtCzynnoscProdukcyjna.Text.Length > 0 && txtPracownik.Text.Length > 0 && txtPracownik.Text.Length > 0 && txtStanowiskoProdukcyjne.Text.Length > 0 && txtSzacowanyCzas.Text.Length > 0 && dtbDataRozpoczecia.Text.Length > 0)
+            {
+                Produkt produkt = new Produkt();
+                produkt.Nazwa_produkt = txtProdukt.Text;
+                
+                Czynnosc_produkcyjna czynnosc_produkcyjna = new Czynnosc_produkcyjna();
+                czynnosc_produkcyjna.Nazwa = txtCzynnoscProdukcyjna.Text;
+                Pracownik pracownik = new Pracownik();
+                pracownik.Nazwisko = txtPracownik.Text;
+                pracownik.Imie = txtImiePracownik.Text;
+                //mergepracownik = pracownik.Nazwisko + "" + pracownik.Imie;
+                Slownik_stanowisko slownik_stanowisko = new Slownik_stanowisko();
+                slownik_stanowisko.Nazwa_stanowiska = txtStanowiskoProdukcyjne.Text;
+                Proces_produkt_czynnosc proces = new Proces_produkt_czynnosc();
+                proces.Czas_trwania = Int32.Parse(txtSzacowanyCzas.Text);
+                Wytwarzanie wytwarzanie = new Wytwarzanie();
+                wytwarzanie.Czas_od = dtbDataRozpoczecia.Value;
+                wytwarzanie.Czas_do = dtbDataEnd.Value;
+                if (dtbDataEnd.Checked)
+                {
+                    wytwarzanie.Czas_do = dtbDataEnd.Value;
+                }
+                db.Produkt.Add(produkt);
+                db.Czynnosc_produkcyjna.Add(czynnosc_produkcyjna);
+                //db.Pracownik.Add(mergepracownik);
+                db.Slownik_stanowisko.Add(slownik_stanowisko);
+                db.Proces_produkt_czynnosc.Add(proces);
+                db.Wytwarzanie.Add(wytwarzanie);
+                db.SaveChanges();
+                cleanTextBox();
+                MessageBox.Show("Dodano nowego klienta!", "Informacja", MessageBoxButtons.OK);
+            }
+        }
+
+
+
+        //private void ComboBox()
+        //{
+        //    System.Data.SqlClient.SqlDataAdapter da = new System.Data.SqlClient.SqlDataAdapter();
+        //    DataSet ds = new DataSet();
+        //    da.Fill(ds);
+        //}
+
+
+
     }
 }
