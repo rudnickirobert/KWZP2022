17218871d46ef485e50d138b73a3661d90629982
LAPTOP-P242TEH4\Mateusz <mateusz.gawron1998@gmail.com>
2022-04-27 14:38:52 +0200

    Editedt Form FormHRDepartmentWorkerContractNew.cs

--- KWZP2022/KWZP2022/KWZPModel.Designer.cs
+++ KWZP2022/KWZP2022/KWZPModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\kamil\Desktop\KWZP ostatatni\KWZP2022\KWZP2022\KWZPModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mateusz\Desktop\nowyBranch\KWZP2022\KWZP2022\KWZP2022\KWZPModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
