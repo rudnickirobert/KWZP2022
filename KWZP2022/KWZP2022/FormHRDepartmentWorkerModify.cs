6d1986e870e7437015d6e1f0934ded82fa26a08b
LAPTOP-P242TEH4\Mateusz <mateusz.gawron1998@gmail.com>
2022-04-27 17:33:05 +0200

    Repaired issue within WorkerModify form

--- KWZP2022/KWZP2022/FormHRDepartmentWorkerModify.cs
+++ KWZP2022/KWZP2022/FormHRDepartmentWorkerModify.cs
@@ -114,47 +114,47 @@ namespace KWZP2022
             {
                 int dane = int.Parse(dgvpracownik.CurrentRow.Cells[0].Value.ToString());
                 Pracownik pracownik = (Pracownik)db.Pracownik.Where(a => a.ID_pracownik == dane).First();
-                tbPracownikNazwisko.Text = pracownik.Nazwisko;
-                tbPracownikImie.Text = pracownik.Imie;
-                tbNrdowodu.Text = pracownik.Nr_dowodu;
-                tbPesel.Text = pracownik.Pesel.ToString();
+                pracownik.Nazwisko = tbPracownikNazwisko.Text;
+                pracownik.Imie = tbPracownikImie.Text;
+                pracownik.Nr_dowodu = tbNrdowodu.Text;
+                pracownik.Pesel = int.Parse(tbPesel.Text);
                 int selectedWyksztalcenie = int.Parse(cbWyksztalcenie.SelectedValue.ToString());
-                selectedWyksztalcenie = pracownik.ID_wyksztalcenie;
+                pracownik.ID_wyksztalcenie = selectedWyksztalcenie;
                 Dane_adresowe_pracownik daneAdresowePracownik = (Dane_adresowe_pracownik)db.Dane_adresowe_pracownik.Single(a => a.ID_pracownik == pracownik.ID_pracownik);
-                pracownik.ID_pracownik = daneAdresowePracownik.ID_pracownik;
-                tbMiejscowosc.Text = daneAdresowePracownik.Miejscowosc;
-                tbUlica.Text = daneAdresowePracownik.Ulica;
-                tbBudynek.Text = daneAdresowePracownik.Nr_budynku;
-                tblokal.Text = daneAdresowePracownik.Nr_lokalu;
-                tbKod.Text = daneAdresowePracownik.Kod_pocztowy;
-                dpracownikod.Value = daneAdresowePracownik.Data_od;
+                daneAdresowePracownik.ID_pracownik = pracownik.ID_pracownik;
+                daneAdresowePracownik.Miejscowosc = tbMiejscowosc.Text;
+                daneAdresowePracownik.Ulica = tbUlica.Text;
+                daneAdresowePracownik.Nr_budynku = tbBudynek.Text;
+                daneAdresowePracownik.Nr_lokalu = tblokal.Text;
+                daneAdresowePracownik.Kod_pocztowy = tbKod.Text;
+                daneAdresowePracownik.Data_od = dpracownikod.Value;
                 if (cbDataDo.Checked)
                 {
-                    dpracownikdo.Value = (DateTime)daneAdresowePracownik.Data_do;
+                    daneAdresowePracownik.Data_do = dpracownikdo.Value;
                 }
                 else
                 {
                     daneAdresowePracownik.Data_do = null;
                 }
                 Nr_telefon_pracownik nrTelefonPracownik = (Nr_telefon_pracownik)db.Nr_telefon_pracownik.Single(a => a.ID_pracownik == pracownik.ID_pracownik);
-                pracownik.ID_pracownik = nrTelefonPracownik.ID_pracownik;
-                tbNumer.Text = nrTelefonPracownik.Numer;
-                dnrod.Value = nrTelefonPracownik.Data_od;
+                nrTelefonPracownik.ID_pracownik = pracownik.ID_pracownik;
+                nrTelefonPracownik.Numer = tbNumer.Text;
+                nrTelefonPracownik.Data_od = dnrod.Value;
                 if (cbDataDoNrTelefonu.Checked)
                 {
-                    dnrdo.Value = (DateTime)nrTelefonPracownik.Data_do;
+                    nrTelefonPracownik.Data_do = dnrdo.Value;
                 }
                 else
                 {
                     nrTelefonPracownik.Data_do = null;
                 }
                 Email_pracownik emailPracownik = (Email_pracownik)db.Email_pracownik.Single(a => a.ID_pracownik == pracownik.ID_pracownik);
-                pracownik.ID_pracownik = emailPracownik.ID_pracownik;
-                tbEmail.Text = emailPracownik.Email;
-                demailod.Value = emailPracownik.Data_od;
+                emailPracownik.ID_pracownik = pracownik.ID_pracownik; 
+                emailPracownik.Email = tbEmail.Text;
+                emailPracownik.Data_od = demailod.Value;
                 if (cbDataDoEmail.Checked)
                 {
-                    demaildo.Value = (DateTime)emailPracownik.Data_do;
+                    emailPracownik.Data_do = demaildo.Value;
                 }
                 else
                 {
